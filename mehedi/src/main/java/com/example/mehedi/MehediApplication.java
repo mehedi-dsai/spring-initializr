1e7e64e3a4e31b304c2d513fcc88b5fd57df935b
mehedi2311 <mehedihasan.green.bd@gmail.com>
2026-04-02 16:41:49 +0600

    Initial commit

---
+++ mehedi/src/main/java/com/example/mehedi/MehediApplication.java
@@ -0,0 +1,13 @@
+package com.example.mehedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MehediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MehediApplication.class, args);
+	}
+
+}
---
+++ mehedi/src/test/java/com/example/mehedi/MehediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mehedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MehediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
